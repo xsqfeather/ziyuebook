703c0969b46149f20495967e88e9c73bdab7da90
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2023-06-13 01:19:33 +0800

    g

--- src/dtos/av.stars.dto.ts
+++ src/dtos/av.stars.dto.ts
@@ -10,7 +10,7 @@ export class CreateAvStarDto {
   @Optional()
   isFemaleFriendly?: boolean;
 
-  @CustomSchema(Joi.string().allow(null, "", undefined))
+  @CustomSchema(Joi.string().allow(null, "").optional())
   introduction!: string;
 
   @Required()
