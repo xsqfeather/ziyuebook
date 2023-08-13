8a90dbffda2835f7b66137dd714f85838bf40720
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2023-08-13 18:30:50 +0800

    pm2 logs

--- src/services/oss.service.ts
+++ src/services/oss.service.ts
@@ -1,46 +1,13 @@
 import { Service } from "typedi";
 import Boom from "@hapi/boom";
-import {
-  createReadStream,
-  existsSync,
-  mkdirSync,
-  readFileSync,
-  statSync,
-} from "fs";
+import { createReadStream, existsSync, mkdirSync, statSync } from "fs";
 
 import ffmpeg from "fluent-ffmpeg";
 import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
 
 @Service()
 export class OssService {
-  getIpfsClient = async () => {
-    try {
-      const { create } = await import("kubo-rpc-client");
-      const client = create("/ip4/127.0.0.1/tcp/5001" as any);
-      return client;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-
   getGunCid() {}
-
-  async addFile(filepath: string) {
-    console.log({ filepath });
-    try {
-      const client = await this.getIpfsClient();
-      const result = await client.add(readFileSync(filepath), {
-        progress: (progress: any) => console.log(progress),
-      });
-      console.log({ result });
-
-      return result;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
 
   async showVideo(filename: string) {
     const filepath = `${process.cwd()}/uploads/${filename}`;
@@ -69,26 +36,6 @@ export class OssService {
       stream,
       size,
     };
-  }
-
-  async addImage(imagePath: string) {
-    try {
-      const { fileTypeFromFile } = await import("file-type");
-      const fileType = await fileTypeFromFile(imagePath);
-      if (!fileType?.mime?.includes("image")) {
-        throw Boom.badRequest("不是图片");
-      }
-      const result = await this.addFile(imagePath);
-      return { imageName: result.path + "." + fileType.ext };
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
-
-  async addVideo(videoPath: string) {
-    const result = await this.addFile(videoPath);
-    return { videoName: result.path + "." + "mp4" };
   }
 
   async showImage(filename: string) {
