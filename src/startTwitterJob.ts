b80a83fa66220de5283d2e927dbaffaa4092fbd1
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2023-09-02 14:44:24 +0800

    task

--- src/startTwitterJob.ts
+++ src/startTwitterJob.ts
@@ -1,4 +1,13 @@
 import Container from "typedi";
 import { TwitterService } from "./services/twitter.service";
+import mongoose from "mongoose";
+import { getMongoURI } from "./lib/config";
 
-Container.get(TwitterService).startAllTask();
+const startTwitterJob = async () => {
+  mongoose.set("strictQuery", true);
+  await mongoose.connect(getMongoURI());
+  const twitterService = Container.get(TwitterService);
+  await twitterService.startAllTask();
+};
+
+startTwitterJob();
