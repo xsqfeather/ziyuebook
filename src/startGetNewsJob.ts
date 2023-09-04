31767e61ebc8a16ab528d50370683a965f7cc281
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2023-09-05 01:37:03 +0800

    deply get article

--- src/startGetNewsJob.ts
+++ src/startGetNewsJob.ts
@@ -215,7 +215,7 @@ const startJob = async () => {
     await getNews("https://www.msn.com/en-us/feed", "en", true);
     await getNews("https://www.msn.com/en-gb/feed", "en", true);
     await getNews("https://www.msn.com/zh-hk/feed", "zhTW", true);
-    await waitTimeout(1000 * 18);
+    await waitTimeout(1000 * 60 * 60);
   }
 };
 
